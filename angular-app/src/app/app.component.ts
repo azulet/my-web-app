54aeb32e5922b3739f64bd006be1436de9ed1b79
azulet <azulet@hotmail.com>
2024-08-21 14:11:03 -0700

    routing

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { UsuarioComponent } from './usuario/usuario.component';
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { LibrosComponent } from './libros/libros.component';
 import { LibroComponent } from './libro/libro.component';
 import { LibrosService } from './services/libros-services.service';
 import { InicioComponent } from './inicio/inicio.component';
-import {AppRoutingModule } from './app.routes';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule,LibrosComponent, LibroComponent, UsuarioComponent, InicioComponent,AppRoutingModule],
+  imports: [RouterOutlet, FormsModule,LibrosComponent, LibroComponent, UsuarioComponent, InicioComponent,AppComponent, RouterLink],
   providers: [LibrosService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- angular-app/src/app/app.routes.ts
+++ angular-app/src/app/app.routes.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 import { InicioComponent } from './inicio/inicio.component';
 import { LibrosComponent } from './libros/libros.component';
 
+
 export const routes: Routes = [
-  { path: '', component: InicioComponent },
+  { path: 'inicio', component: InicioComponent },
   { path: 'libros', component: LibrosComponent }
 ];
 
