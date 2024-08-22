3744fbc19b909f907102418beb230974b1da948c
azulet <azulet@hotmail.com>
2024-08-21 17:29:26 -0700

    add angular material

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -6,13 +6,13 @@ import { LibrosComponent } from './libros/libros.component';
 import { LibroComponent } from './libro/libro.component';
 import { LibrosService } from './services/libros-services.service';
 import { InicioComponent } from './inicio/inicio.component';
-
+import { MaterialComponent } from './material.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule,LibrosComponent, LibroComponent, UsuarioComponent, InicioComponent,AppComponent, RouterLink],
+  imports: [MaterialComponent, RouterOutlet, FormsModule,LibrosComponent, LibroComponent, UsuarioComponent, InicioComponent,AppComponent, RouterLink],
   providers: [LibrosService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
